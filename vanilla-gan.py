1694d1f21aeba5028b23ccbd0b57f6e67666590d
S.P. Mohanty <spmohanty91@gmail.com>
2018-02-26 06:35:00 +0100

    Working version

--- vanilla-gan.py
+++ vanilla-gan.py
@@ -7,6 +7,7 @@ from torchvision import transforms, datasets
 import math
 
 DATA_FOLDER = './torch_data/VGAN/MNIST'
+torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
 def mnist_data():
     compose = transforms.Compose(
@@ -21,11 +22,10 @@ def mnist_data():
 data = mnist_data()
 data_loader = torch.utils.data.DataLoader(data, batch_size=100, shuffle=True)
 num_batches = len(data_loader)
-print(data)
 
 DISCRIMINATOR_INPUT_SIZE = 784
 NOISE_SIZE = 100
-class Discriminator:
+class Discriminator(torch.nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.n_features = DISCRIMINATOR_INPUT_SIZE
@@ -73,7 +73,7 @@ def vectors_to_images(vector):
 
 
 
-class Generator:
+class Generator(torch.nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
@@ -110,3 +110,89 @@ def noise(size):
     n = Variable(torch.randn(size, 100))
     if torch.cuda.is_available(): return n.cuda()
     return n
+
+
+# Instantiate networks
+discriminator = Discriminator()
+generator = Generator()
+
+if torch.cuda.is_available():
+    discriminator = discriminator.cuda()
+    generator = generator.cuda()
+    # discriminator = nn.DataParallel(discriminator)
+    # generator = nn.DataParallel(generator)
+
+
+# Instantiate optimizers
+d_optimizer = optim.Adam(discriminator.parameters(), lr=0.0002)
+g_optimizer = optim.Adam(generator.parameters(), lr=0.0002)
+
+# Loss function
+loss = nn.BCELoss()
+d_steps = 1 #Number of steps to train the discriminator in every batch
+num_epochs = 200
+
+
+def real_data_target(size):
+    data = Variable(torch.ones(size, 1))
+    if torch.cuda.is_available(): return data.cuda()
+    return data
+
+def fake_data_target(size):
+    data = Variable(torch.zeros(size, 1))
+    if torch.cuda.is_available(): return data.cuda()
+    return data
+
+def train_discriminator(optimizer, real_data, fake_data):
+    # reset gradients
+    optimizer.zero_grad()
+
+    # Train on real data
+    prediction_real = discriminator(real_data)
+    # Calculate error and back propagate
+    error_real = loss(prediction_real, real_data_target(real_data.size(0)))
+    error_real.backward()
+
+
+    # Train on fake data
+    prediction_fake = discriminator(fake_data)
+    # Calculate error and back propagate
+    error_fake = loss(prediction_fake, fake_data_target(fake_data.size(0)))
+    error_fake.backward()
+
+
+    optimizer.step()
+
+    return error_real + error_fake, prediction_real, prediction_fake
+
+def train_generator(optimizer, fake_data):
+    optimizer.zero_grad()
+
+    prediction = discriminator(fake_data)
+    error = loss(prediction, real_data_target(prediction.size(0)))
+    error.backward()
+
+    optimizer.step()
+    return error
+
+
+num_test_samples = 16
+test_noise =    noise(num_test_samples)
+
+
+for epoch in range(num_epochs):
+    for n_batch, (real_batch, _) in enumerate(data_loader):
+
+        real_data = Variable(images_to_vectors(real_batch))
+        if torch.cuda.is_available(): real_data = real_data.cuda()
+
+        fake_data = generator(noise(real_data.size(0))).detach()
+
+        # train Discriminator
+        d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer, real_data, fake_data)
+
+        # Train Generator
+        fake_data = generator(noise(real_batch.size(0)))
+        g_error = train_generator(g_optimizer, fake_data)
+
+        print(d_error, g_error, epoch, n_batch, num_batches)
